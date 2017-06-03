32662c5ad92ebcccf25658db0d8de6a23175cc02
Adrien Zaganelli <adrienzaganelli@gmail.com>
2017-06-03 16:28:11 +0200

    :art: move loginActivity and link it to home

--- app/src/main/java/com/blup/android/blup/activity/LoginActivity.java
+++ app/src/main/java/com/blup/android/blup/activity/LoginActivity.java
@@ -1,8 +1,9 @@
-package com.blup.android.blup;
+package com.blup.android.blup.activity;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -19,6 +20,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -28,6 +30,8 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.blup.android.blup.R;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -76,8 +80,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
                 if (id == R.id.login || id == EditorInfo.IME_NULL) {
                     attemptLogin();
+                    Log.d("LoginActivity", "dlzpaldpaz");
                     return true;
                 }
+                Log.d("LoginActivity", "dlzpaldpaz");
                 return false;
             }
         });
@@ -186,7 +192,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             // perform the user login attempt.
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
-            mAuthTask.execute((Void) null);
+           // mAuthTask.execute((Void) null);
+            Intent i = new Intent(LoginActivity.this, HomeActivity.class);
+            startActivity(i);
         }
     }
 
