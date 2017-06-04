716edbaff890cd3c67db8198870464d33f58a3e7
Adrien Zaganelli <adrienzaganelli@gmail.com>
2017-06-05 01:39:13 +0200

    :art: init item focus activity

---
+++ app/src/main/java/com/blup/android/blup/activity/ItemFocusActivity.java
@@ -0,0 +1,15 @@
+package com.blup.android.blup.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.blup.android.blup.R;
+
+public class ItemFocusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_focus);
+    }
+}
