21f2242a2f241f7ffdfc4c1e110246ba3e54045c
LaurenceRouge <laurence.rouge@hetic.net>
2017-06-07 20:05:35 +0200

    :art: set dynamic item title

--- app/src/main/java/com/blup/android/blup/adaptateritem/MyAdapter.java
+++ app/src/main/java/com/blup/android/blup/adaptateritem/MyAdapter.java
@@ -1,35 +1,27 @@
 package com.blup.android.blup.adaptateritem;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.Context;
+
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
-import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
+
 import com.blup.android.blup.R;
 import com.blup.android.blup.activity.HomeActivity;
 import com.blup.android.blup.activity.ItemFocusActivity;
 
-import org.json.JSONArray;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.Arrays;
+
 import java.util.List;
-import java.util.Objects;
+
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
@@ -118,7 +110,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
                @Override
                public void onClick(View view) {
                    Intent intent = new Intent(view.getContext(), ItemFocusActivity.class);
-                   intent.putExtra("user_id", currentItemId);
+                   intent.putExtra("item_id", currentItemId);
                    view.getContext().startActivity(intent);
                }
            });
