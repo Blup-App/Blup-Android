6bff5ec2be62675efae660f9ffdf5a41f2ccd842
Adrien Zaganelli <adrienzaganelli@gmail.com>
2017-06-07 19:49:36 +0200

    fghjlkmù

--- app/src/main/java/com/blup/android/blup/activity/ItemFocusActivity.java
+++ app/src/main/java/com/blup/android/blup/activity/ItemFocusActivity.java
@@ -2,6 +2,8 @@ package com.blup.android.blup.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.blup.android.blup.R;
 
@@ -11,5 +13,12 @@ public class ItemFocusActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_focus);
+
+        TextView title = (TextView) findViewById(R.id.item_title);
+        Bundle extras = getIntent().getExtras();
+
+        String itemId = getIntent().getStringExtra("item_id");
+
+        title.setText(itemId);
     }
 }
--- app/src/main/java/com/blup/android/blup/adaptateritem/MyAdapter.java
+++ app/src/main/java/com/blup/android/blup/adaptateritem/MyAdapter.java
@@ -4,11 +4,13 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -32,7 +34,6 @@ import java.util.Objects;
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     private String userId;
-
     public List getItemsList() {
         return itemsList;
     }
@@ -57,20 +58,6 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
 
 
-    private final List<Pair<String, String>> characters = Arrays.asList(
-           Pair.create("Lyra Belacqua", "Brave, curious, and crafty, she has been prophesied by the witches to help the balance of life"),
-           Pair.create("Pantalaimon", "Lyra's daemon, nicknamed Pan."),
-           Pair.create("Roger Parslow", "Lyra's friends"),
-           Pair.create("Lord Asriel", "Lyra's uncle"),
-           Pair.create("Marisa Coulter", "Intelligent and beautiful, but extremely ruthless and callous."),
-           Pair.create("Iorek Byrnison", "Armoured bear, Rightful king of the panserbjørne. Reduced to a slave of the human village Trollesund."),
-           Pair.create("Serafina Pekkala", "Witch who closely follows Lyra on her travels."),
-           Pair.create("Lee Scoresby", "Texan aeronaut who transports Lyra in his balloon. Good friend with Iorek Byrnison."),
-           Pair.create("Ma Costa", "Gyptian woman whose son, Billy Costa is abducted by the \"Gobblers\"."),
-           Pair.create("John Faa", "The King of all gyptian people.")
-   );
-
-
 
    @Override
    public int getItemCount() {
@@ -87,28 +74,51 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
    @Override
    public void onBindViewHolder(MyViewHolder holder, int position) {
        Object pair = itemsList.get(position);
+       try {
+           JSONObject item = new JSONObject(pair.toString());
+           holder.setCurrentItemId(item.getString("id"));
+           holder.display(pair.toString());
 
-       holder.display(pair.toString());
+       } catch (JSONException e) {
+           e.printStackTrace();
+       }
    }
 
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
 
-       private final TextView name;
-       private final TextView description;
+       private final TextView title;
+       private final TextView date;
+       private final ImageView preview;
+
+        public void setCurrentItemId(String currentItemId) {
+            this.currentItemId = currentItemId;
+        }
+
+        private String currentItemId;
+
+        public void setItemId(String itemId) {
+            this.itemId = itemId;
+        }
+
+        private String itemId;
 
        private JSONObject currentPair;
 
        public MyViewHolder(final View itemView) {
            super(itemView);
 
-           name = ((TextView) itemView.findViewById(R.id.title));
-           description = ((TextView) itemView.findViewById(R.id.date));
+           title = ((TextView) itemView.findViewById(R.id.title));
+           date = ((TextView) itemView.findViewById(R.id.date));
+           preview = ((ImageView) itemView.findViewById(R.id.preview));
+
+           final Intent oldIntent = ((HomeActivity) itemView.getContext()).getIntent();
 
            itemView.setOnClickListener(new View.OnClickListener() {
                @Override
                public void onClick(View view) {
-                    Intent intent = new Intent(view.getContext(), ItemFocusActivity.class);
+                   Intent intent = new Intent(view.getContext(), ItemFocusActivity.class);
+                   intent.putExtra("user_id", currentItemId);
                    view.getContext().startActivity(intent);
                }
            });
@@ -117,7 +127,11 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
        public void display(String pair) {
            try {
                JSONObject currentPair = new JSONObject(pair);
-               description.setText(currentPair.getString("title"));
+
+               title.setText(currentPair.getString("title"));
+               date.setText(currentPair.get("date").toString());
+               preview.setImageURI(Uri.parse(currentPair.getString("img")));
+
 
 
            } catch (JSONException e) {
